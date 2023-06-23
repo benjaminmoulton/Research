e020c4fd19ab16132efc194ffa1de61589b4fb50
Ben <moulton.benjamin.c@gmail.com>
2023-06-22 21:10:35 -0600

    cylinder work...

--- SciTech_2023/symbolic/tapered_elliptical_cylinder_inertia.py
+++ SciTech_2023/symbolic/tapered_elliptical_cylinder_inertia.py
@@ -47,8 +47,8 @@ if __name__ == "__main__":
     theta_bnd = (t,theta_lo,theta_up)
 
     i = x
-    j = rp * cos(t)
-    k = rp * sin(t)
+    j = rp# * cos(t)
+    k = rp# * sin(t)
 
     V = [
         [0,0,0],
@@ -133,6 +133,39 @@ if __name__ == "__main__":
         else:
             ii0 = simp( V_ints[momlisti[q-7][0]].subs({ay0:ay1,az0:az1}) )
             ii1 = simp( V_ints[momlisti[q-7][1]].subs({ay0:ay1,az0:az1}) )
+            cg0 = cg[momcglist[q-7][0]]
+            cg1 = cg[momcglist[q-7][1]]
+            I = simp(mp*( (ii0+ii1)/m - cg0**2 - cg1**2) )
+            print("I{} = {}".format(momlist[q-7],I))
+        
+    print()
+    print()
+
+    print("tapered cylinder eqs...")
+    m = simp( V_ints[0].subs({az0:ay0,az1:ay1}) )
+    cglist = ["x","y","z"]
+    prodlist = ["xy","xz","yz"]
+    prodcglist = [(0,1),(0,2),(1,2)]
+    momlist = ["xx","yy","zz"]
+    momlisti = [(8,9),(7,9),(7,8)]
+    momcglist = [(1,2),(0,2),(0,1)]
+    cg = []
+
+    for q in range(len(V)):
+        iii = simp( V_ints[q].subs({az0:ay0,az1:ay1}) )
+        if q == 0:
+            print("V = {}".format(simp(m/p)))
+            print("m = {}".format(m))
+        elif q < 4:
+            cg.append(simp(iii/m))
+            print("{}-cg = {}".format(cglist[q-1],cg[-1]))
+        elif q < 7:
+            cgprod = cg[prodcglist[q-4][0]] * cg[prodcglist[q-4][1]]
+            I = simp(mp*(iii/m - cgprod))
+            print("I{} = {}".format(prodlist[q-4],I))
+        else:
+            ii0 = simp( V_ints[momlisti[q-7][0]].subs({az0:ay0,az1:ay1}) )
+            ii1 = simp( V_ints[momlisti[q-7][1]].subs({az0:ay0,az1:ay1}) )
             cg0 = cg[momcglist[q-7][0]]
             cg1 = cg[momcglist[q-7][1]]
             I = simp(mp*( (ii0+ii1)/m - cg0**2 - cg1**2) )
@@ -236,3 +269,6 @@ if __name__ == "__main__":
             cg1 = cg[momcglist[q-7][1]]
             I = simp(mp*( (ii0+ii1)/m - cg0**2 - cg1**2) )
             print("I{} = {}".format(momlist[q-7],I))
+    
+    print()
+    print()
